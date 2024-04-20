3250b2633c0d985a2b27d26ee8777610e8d423dc
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2024-04-20 22:58:10 +0400

    add new user

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,22 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { createUser } from "../util/auth";
+import LoadingOverloay from "../components/ui/LoadingOverlay";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuth, setIsAuth] = useState(false);
+
+  async function signUpHandler({ email, password }) {
+    setIsAuth(true);
+    await createUser(email, password);
+    setIsAuth(false);
+  }
+
+  if (isAuth) {
+    return <LoadingOverloay message="Creating User..." />;
+  }
+
+  return <AuthContent onAuthenticate={signUpHandler} />;
 }
 
 export default SignupScreen;
