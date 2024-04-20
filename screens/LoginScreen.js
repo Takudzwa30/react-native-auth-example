5fe203b06eeaa8860a2b75e8799103910e41bfe0
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2024-04-20 23:17:04 +0400

    added login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,22 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { logIn } from "../util/auth";
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuth, setIsAuth] = useState(false);
+
+  async function loginHandler({ email, password }) {
+    setIsAuth(true);
+    await logIn(email, password);
+    setIsAuth(false);
+  }
+
+  if (isAuth) {
+    return <LoadingOverlay message="Logging you in..." />;
+  }
+
+  return <AuthContent onAuthenticate={loginHandler} isLogin />;
 }
 
 export default LoginScreen;
