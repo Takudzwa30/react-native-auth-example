0a3bd4765e986ea2819f6be88efe24eedc358679
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2024-04-20 23:57:30 +0400

    added functionality sign up and signin

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,26 +1,29 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { logIn } from "../util/auth";
 import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function LoginScreen() {
-  const [isAuth, setIsAuth] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const authCtx = useContext(AuthContext);
 
   async function loginHandler({ email, password }) {
-    setIsAuth(true);
+    setIsAuthenticating(true);
     try {
-      await logIn(email, password);
+      const token = await logIn(email, password);
+      authCtx.authenticate(token);
     } catch (error) {
       Alert.alert(
         "Authentication failed!",
         "Could not login, please check your credentials or try again later"
       );
+      setIsAuthenticating(false);
     }
-    setIsAuth(false);
   }
 
-  if (isAuth) {
+  if (isAuthenticating) {
     return <LoadingOverlay message="Logging you in..." />;
   }
 
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,24 +1,27 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import { createUser } from "../util/auth";
 import LoadingOverloay from "../components/ui/LoadingOverlay";
 import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function SignupScreen() {
   const [isAuth, setIsAuth] = useState(false);
 
+  const authCtx = useContext(AuthContext);
+
   async function signUpHandler({ email, password }) {
     setIsAuth(true);
     try {
-      await createUser(email, password);
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
     } catch (error) {
       Alert.alert(
         "Authentication failed!",
         "Could not create user, please check your input and try again later"
       );
+      setIsAuth(false);
     }
-
-    setIsAuth(false);
   }
 
   if (isAuth) {
